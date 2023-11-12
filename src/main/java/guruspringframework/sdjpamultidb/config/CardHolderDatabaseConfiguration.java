49d9a3672008f8fabafac08da3159f67637365d2
echcherrate.ismail <echcherrate.ismail@gmail.com>
2023-11-12 16:19:30 +0100

    Flyway datasource config, custom migrations

--- src/main/java/guruspringframework/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
+++ src/main/java/guruspringframework/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
@@ -2,13 +2,13 @@ package guruspringframework.sdjpamultidb.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import guruspringframework.sdjpamultidb.domain.cardholder.CreditCardHolder;
-import guruspringframework.sdjpamultidb.domain.creditcard.CreditCard;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -18,6 +18,8 @@ import javax.sql.DataSource;
 /**
  * Created by EI on 11/12/2023
  */
+@EnableJpaRepositories(basePackages = "guruspringframework.sdjpamultidb.repositories.cardholder",entityManagerFactoryRef = "cardHolderEntityManagerFactory", transactionManagerRef = "cardHolderTransactionManager")
+
 @Configuration
 public class CardHolderDatabaseConfiguration {
 
---
+++ src/main/java/guruspringframework/sdjpamultidb/config/FlywayConfiguration.java
@@ -0,0 +1,57 @@
+package guruspringframework.sdjpamultidb.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by EI on 11/12/2023
+ */
+
+@Configuration
+public class FlywayConfiguration {
+
+
+    @Bean
+    @ConfigurationProperties("spring.card.flyway")
+    public DataSourceProperties cardFlywayDataSourceProps() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flywayCard(@Qualifier("cardFlywayDataSourceProps") DataSourceProperties cardFlywayDataSourceProps) {
+        return Flyway.configure().dataSource(cardFlywayDataSourceProps.getUrl(), cardFlywayDataSourceProps.getUsername(), cardFlywayDataSourceProps.getPassword())
+                .locations("classpath:/db/migrations/card")
+                .load();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.cardholder.flyway")
+    public DataSourceProperties cardHolderFlywayDataSourceProps() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flywayCardHolder(@Qualifier("cardHolderFlywayDataSourceProps") DataSourceProperties cardHolderFlywayDataSourceProps) {
+        return Flyway.configure().dataSource(cardHolderFlywayDataSourceProps.getUrl(), cardHolderFlywayDataSourceProps.getUsername(), cardHolderFlywayDataSourceProps.getPassword())
+                .locations("classpath:/db/migrations/cardholder")
+                .load();
+    }
+
+
+    @Bean
+    @ConfigurationProperties("spring.pan.flyway")
+    public DataSourceProperties panFlywayDataSourceProps() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flywayPan(@Qualifier("panFlywayDataSourceProps") DataSourceProperties panFlywayDataSourceProps) {
+        return Flyway.configure().dataSource(panFlywayDataSourceProps.getUrl(), panFlywayDataSourceProps.getUsername(), panFlywayDataSourceProps.getPassword())
+                .locations("classpath:/db/migrations/pan")
+                .load();
+    }
+}
